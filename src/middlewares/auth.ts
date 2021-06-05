adbd2c9dbd844df656aff4bdb7d9ecaea9fa32c8
Androz2091 <androz2091@gmail.com>
2021-06-05 11:06:17 +0200

    :sparkles: Verify revoked jwt

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,8 +1,9 @@
 import { DecodedUserJWT, DecodedGuildJWT, decodeJWT } from '../utils/jwt'
 import { NextFunction, Response, Request } from "express";
 import { replyError } from "..";
+import database from '../database'
 
-export default (req: Request, res: Response, next: NextFunction): void => {
+export default async (req: Request, res: Response, next: NextFunction): Promise<void> => {
 
     const apiKey = req.headers['authorization']
     if (!apiKey) return replyError(403, 'Missing Authorization header', res);
@@ -20,9 +21,13 @@ export default (req: Request, res: Response, next: NextFunction): void => {
         const key = apiKey.slice(6, apiKey.length);
         const data = decodeJWT(key) as DecodedGuildJWT|null;
         if (!data) return replyError(403, 'Unauthorized. Guild JWT can not be verified.', res);
+        const revokedJWTs = await database.fetchRevokedJWTs();
+        if (revokedJWTs.includes(key)) return replyError(403, 'Unauthorized. Guild JWT has been revoked.', res);
         req.jwt = key;
         req.jwtType = 'guild';
         req.decodedJWT = data;
+    } else {
+        return replyError(403, 'Unauthorized. JWT can not be verified.', res);
     }
 
     next();
