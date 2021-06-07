5f959ed0cb23fbe7fee0a5f82892ffbc0a91567c
Androz2091 <androz2091@gmail.com>
2021-06-07 17:55:19 +0200

    :sparkles: Add missing endpoint

--- src/routes/guilds.routes.ts
+++ src/routes/guilds.routes.ts
@@ -273,7 +273,7 @@ guildsRouter.get('/:guildID/members/:userID', auth, permissions, premium, checkS
     const settings = await database.fetchGuildSettings(guildID);
     const memberData = await database.fetchGuildMember({
         userID,
-        guildID,    
+        guildID,
         storageID: settings.storageID
     });
 
@@ -342,6 +342,35 @@ guildsRouter.post('/:guildID/members/:userID', auth, permissions, premium, check
     });
 
     replyData(newMemberData, req, res);
+
+});
+
+guildsRouter.get('/:guildID/members/:userID/events', auth, permissions, premium, checkSchema({
+    userID: {
+        in: 'params',
+        isString: true,
+        matches: {
+            options: DISCORD_ID_REGEX
+        }
+    }
+}), async (req: Request, res: Response) => {
+
+    const err = validationResult(req);
+    if (!err.isEmpty()) {
+        const errors = err.mapped();
+        const msg = errors[Object.keys(errors)[0]].msg;
+        return replyError(400, msg, res);
+    }
+
+    const guildID = req.params.guildID;
+    const userID = req.params.userID;
+
+    const memberData = await database.fetchGuildMemberEvents({
+        userID,
+        guildID
+    });
+
+    replyData(memberData, req, res);
 
 });
 
