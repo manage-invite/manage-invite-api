a1ddbc5e319dd5d7fc06ea6d1edce60af019c4d1
Androz2091 <androz2091@gmail.com>
2021-06-07 17:53:57 +0200

    :sparkles: Add some missing endpoints

--- src/routes/guilds.routes.ts
+++ src/routes/guilds.routes.ts
@@ -250,6 +250,101 @@ guildsRouter.post('/:guildID/storages', auth, permissions, premium, async (req, 
 
 });
 
+guildsRouter.get('/:guildID/members/:userID', auth, permissions, premium, checkSchema({
+    userID: {
+        in: 'params',
+        isString: true,
+        matches: {
+            options: DISCORD_ID_REGEX
+        }
+    }
+}), async (req: Request, res: Response) => {
+
+    const err = validationResult(req);
+    if (!err.isEmpty()) {
+        const errors = err.mapped();
+        const msg = errors[Object.keys(errors)[0]].msg;
+        return replyError(400, msg, res);
+    }
+
+    const guildID = req.params.guildID;
+    const userID = req.params.userID;
+
+    const settings = await database.fetchGuildSettings(guildID);
+    const memberData = await database.fetchGuildMember({
+        userID,
+        guildID,    
+        storageID: settings.storageID
+    });
+
+    replyData(memberData, req, res);
+
+});
+
+guildsRouter.post('/:guildID/members/:userID', auth, permissions, premium, checkSchema({
+    userID: {
+        in: 'params',
+        isString: true,
+        matches: {
+            options: DISCORD_ID_REGEX
+        }
+    },
+    number: {
+        in: 'body',
+        isInt: true
+    },
+    type: {
+        in: 'body',
+        isString: true,
+        custom: {
+            options: (value) => ['regular', 'bonus', 'fake', 'leaves'].includes(value)
+        }
+    }
+}), async (req: Request, res: Response) => {
+
+    const err = validationResult(req);
+    if (!err.isEmpty()) {
+        const errors = err.mapped();
+        const msg = errors[Object.keys(errors)[0]].msg;
+        return replyError(400, msg, res);
+    }
+
+    const guildID = req.params.guildID;
+    const userID = req.params.userID;
+
+    const number = req.body.number;
+    const type = req.body.type;
+
+    const settings = await database.fetchGuildSettings(guildID);
+
+    const memberData = await database.fetchGuildMember({
+        userID,
+        guildID,
+        storageID: settings.storageID
+    });
+    if (memberData.notCreated) await database.createGuildMember({
+        userID,
+        guildID,
+        storageID: settings.storageID
+    });
+    await database.addInvites({
+        userID,
+        guildID,
+        storageID: settings.storageID,
+        number,
+        type
+    });
+
+    const newMemberData = await database.fetchGuildMember({
+        userID,
+        guildID,
+        storageID: settings.storageID
+    });
+
+    replyData(newMemberData, req, res);
+
+});
+
 guildsRouter.get('/:guildID/plugins', auth, permissions, premium, async (req, res) => {
 
     const guildID = req.params.guildID;
