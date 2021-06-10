27c7f810eee868eb63161526c996889db76a12bf
Androz2091 <androz2091@gmail.com>
2021-06-10 22:13:43 +0200

    :wrench: Add pg host to config

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,7 @@
 import DatabaseHandler from '@manage-invite/manage-invite-db-client';
 
 const handler = new DatabaseHandler({}, {
+    host: process.env.PG_HOST,
     database: process.env.PG_DATABASE,
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD
