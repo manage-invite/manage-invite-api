76daf3484051789cebd388d7e15caecc91f1e962
Androz2091 <androz2091@gmail.com>
2021-06-12 08:15:48 +0200

    :bug: Fix shard calculation

--- src/utils/discord.ts
+++ src/utils/discord.ts
@@ -1,1 +1,1 @@
-export const getShardOf = (guildID: string): number => (guildID as unknown as number >> 22) % (process.env.SHARD_COUNT as unknown as number);
+export const getShardOf = (guildID: string): number => Number(BigInt(guildID as unknown as number) >> 22n) % (process.env.SHARD_COUNT as unknown as number);
