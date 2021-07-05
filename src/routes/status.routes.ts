af2324b3e27608a2d08e63958d9b92a39050b95a
Androz2091 <androz2091@gmail.com>
2021-07-05 17:46:18 +0200

    :sparkles: Add uptime in status response

--- src/routes/status.routes.ts
+++ src/routes/status.routes.ts
@@ -1,14 +1,33 @@
 import { Router } from 'express';
 import { replyData } from '..';
 import { getShardsStatus } from '../ipc-server';
+import database from '../database';
+import fetch from 'node-fetch';
 
 const statusRouter = Router();
 
 statusRouter.get('/shards', async (req, res) => {
 
+    let uptime = await database.redis.getString('uptime');
+
+    if (!uptime) {
+        const watchbotStatus = await (await fetch(`https://api.watchbot.app/bot/${process.env.CLIENT_ID}`, {
+            headers: {
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                'AUTH-TOKEN': process.env.WATCHBOT_API_KEY!
+            }
+        })).json();
+        uptime = watchbotStatus['7d'];
+        database.redis.setString('uptime', uptime);
+        database.redis.client.expire('uptime', 100);
+    }
+
     const shardsStatuses = await getShardsStatus();
 
-    replyData(shardsStatuses, req, res);
+    replyData({
+        uptime,
+        shards: shardsStatuses
+    }, req, res);
 });
 
 export default statusRouter;
