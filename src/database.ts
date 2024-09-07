87aa97f046df9508b491d13f7cf89a18401a36f9
Androz 2091 <androz2091@gmail.com>
2024-09-07 22:15:34 +0200

    update env

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,12 @@
-import DatabaseHandler from '@manage-invite/manage-invite-db-client';
+import DatabaseHandler from '@androz2091/manage-invite-db-client';
 
-const handler = new DatabaseHandler({}, {
+const handler = new DatabaseHandler({
+    port: process.env.REDIS_PORT, // Redis port
+    host: process.env.REDIS_HOST, // Redis host
+    username: process.env.REDIS_USER, // Redis username`
+    password: process.env.REDIS_PASSWORD, // Redis password
+    db: 0, // Defaults to 0
+}, {
     host: process.env.PG_HOST,
     database: process.env.PG_DATABASE,
     user: process.env.PG_USER,
